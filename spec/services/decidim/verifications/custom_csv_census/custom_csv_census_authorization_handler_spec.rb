4cd952aa025df8f0c5fbc3367088b44f37e4037b
Oliver Valls <oliver.vh@coditramuntana.com>
2020-07-09 16:11:35 +0200

    Apply optional encoding of datum fields to import process

--- spec/services/decidim/verifications/custom_csv_census/custom_csv_census_authorization_handler_spec.rb
+++ spec/services/decidim/verifications/custom_csv_census/custom_csv_census_authorization_handler_spec.rb
@@ -8,15 +8,18 @@ module Decidim
       describe CustomCsvCensusAuthorizationHandler do
         subject { described_class.from_params(params) }
 
-        let(:params) { { id_document: id_document, user: user } }
+        let(:params) { { id_document: id_document, favourite_color: "pink", birth_date: birth_date, user: user } }
         let(:id_document) { "00000000Z" }
+        let(:birth_date) { "01/02/2020".to_date }
         let(:user) { create(:user) }
         let(:organization) { user.organization }
 
         let!(:data) do
           create(
             :census_datum,
-            id_document: CensusDatum.encode("00000000Z"),
+            id_document: CensusDatum.encode(id_document),
+            favourite_color: "purple",
+            birth_date: birth_date,
             organization: organization
           )
         end
--- spec/support/csv_context.rb
+++ spec/support/csv_context.rb
@@ -3,9 +3,9 @@
 require "csv"
 
 shared_context "with tmp csv file" do
-  let(:headers) { "id_document" }
-  let(:row2) { "00000000Z" }
-  let(:row3) { "00000000Z" }
+  let(:headers) { ["id_document", "favourite_color", "birth_date"] }
+  let(:row2) { ["00000000Z", "yellow", "01/01/2020"] }
+  let(:row3) { ["00000000Z", "green", "01/01/2020"] }
   let(:rows) { [headers, row2, row3] }
   let(:tmp_csv_path) { "/tmp/test.csv" }
   let(:tmp_csv_file) { double(path: Pathname.new(tmp_csv_path), original_filename: "test.csv") }
@@ -13,7 +13,7 @@ shared_context "with tmp csv file" do
   before(:each) do
     CSV.open(tmp_csv_path, "w") do |csv|
       rows.each do |row|
-        csv << row.split(",")
+        csv << row
       end
     end
   end
